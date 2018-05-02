169ff1723009b4b6b0d44f22a779b10575a1597c
Théo Pichon <theopichon23@gmail.com>
2018-05-02 17:29:25 +0200

    commit nous en sommes à occupeLaPosition public boolean :rocket:

--- Desktop/Java dev/spaceinvaders/src/main/java/fr/unilim/uml/spaceinvaders/SpaceInvaders.java
+++ Desktop/Java dev/spaceinvaders/src/main/java/fr/unilim/uml/spaceinvaders/SpaceInvaders.java
@@ -3,7 +3,7 @@ package fr.unilim.uml.spaceinvaders;
 public class SpaceInvaders {
 	int longueur;
 	int hauteur;
-
+	Vaisseau vaisseau;
 	public SpaceInvaders(int longueur, int hauteur) {
 		this.longueur = longueur;
 		this.hauteur = hauteur;
@@ -11,12 +11,18 @@ public class SpaceInvaders {
     @Override
     public String toString() {
         StringBuilder espaceDeJeu = new StringBuilder();
-        for (int i = 0; i < hauteur; i++) {
-            for (int j = 0; j < longueur; j++) {
-                espaceDeJeu.append('.');
+        for (int y = 0; y < hauteur; y++) {
+            for (int x = 0; x < longueur; x++) {
+            	  if (vaisseau.occupeLaPosition(x, y))
+            		     espaceDeJeu.append('V');
+            		else
+            			espaceDeJeu.append('.');
             }
             espaceDeJeu.append('\n');
         }
         return espaceDeJeu.toString();
     }
+	public void positionnerUnNouveauVaisseau(int x, int y) {
+		 this.vaisseau = new Vaisseau (x,y);
+	}
 }
