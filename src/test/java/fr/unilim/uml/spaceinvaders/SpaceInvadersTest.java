5fe2828248f44769620297fdaa74ca8616fbb955
Sandjay Thiagaradja <sandjay.thiagaradja@etu.unilim.fr>
2018-05-15 11:21:23 +0200

    Dimensionner le vaisseau;Fonctionnalité2 fin étape 1 :rocket:

--- src/test/java/fr/unilim/uml/spaceinvaders/SpaceInvadersTest.java
+++ src/test/java/fr/unilim/uml/spaceinvaders/SpaceInvadersTest.java
@@ -34,8 +34,64 @@ public class SpaceInvadersTest {
         }
    
    @Test
+	public void test_VaisseauAvance_DeplacerVaisseauVersLaGauche() {
+		
+		spaceinvaders.positionnerUnNouveauVaisseau(1,1,7,9);
+
+		spaceinvaders.deplacerVaisseauVersLaGauche();
+		
+		assertEquals("" + 
+		"...............\n" + 
+		"...............\n" +
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"......V........\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
+	}
+   
+   @Test
+  	public void test_unNouveauVaisseauAvecDimensionEstCorrectementPositionneDansEspaceJeu() {
+  		spaceinvaders.positionnerUnNouveauVaisseau(3,2,7,9);
+  		assertEquals("" + 
+  		"...............\n" + 
+  		"...............\n" +
+  		"...............\n" + 
+  		"...............\n" + 
+  		"...............\n" + 
+  		"...............\n" + 
+  		"...............\n" + 
+  		"...............\n" + 
+  		".......VVV.....\n" + 
+  		".......VVV.....\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
+  	}
+   
+   @Test
+	public void test_VaisseauImmobile_DeplacerVaisseauVersLaGauche() {
+		
+		spaceinvaders.positionnerUnNouveauVaisseau(1,1,0,9);
+
+		spaceinvaders.deplacerVaisseauVersLaGauche();
+		
+		assertEquals("" + 
+		"...............\n" + 
+		"...............\n" +
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"...............\n" + 
+		"V..............\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
+	}
+   
+   @Test
 	public void test_unNouveauVaisseauEstCorrectementPositionneDansEspaceJeu() {
-		spaceinvaders.positionnerUnNouveauVaisseau(7,9);
+		spaceinvaders.positionnerUnNouveauVaisseau(1,1,7,9);
 		assertEquals("" + 
 		"...............\n" + 
 		"...............\n" +
@@ -51,7 +107,7 @@ public class SpaceInvadersTest {
    
    @Test
    public void test_VaisseauAvance_DeplacerVaisseauVersLaDroite() {
-   	spaceinvaders.positionnerUnNouveauVaisseau(7,9);
+   	spaceinvaders.positionnerUnNouveauVaisseau(1,1,7,9);
   	
    	spaceinvaders.deplacerVaisseauVersLaDroite();
    	
@@ -71,7 +127,7 @@ public class SpaceInvadersTest {
    @Test
 	public void test_VaisseauImmobile_DeplacerVaisseauVersLaDroite() {
 		
-		spaceinvaders.positionnerUnNouveauVaisseau(14,9);
+		spaceinvaders.positionnerUnNouveauVaisseau(1,1,14,9);
 
 		spaceinvaders.deplacerVaisseauVersLaDroite();
 		
@@ -92,28 +148,28 @@ public class SpaceInvadersTest {
 	public void test_UnNouveauVaisseauPositionneHorsEspaceJeu_DoitLeverUneException() {
 		
 		try {
-			spaceinvaders.positionnerUnNouveauVaisseau(15,9);
+			spaceinvaders.positionnerUnNouveauVaisseau(1,1,15,9);
 			fail("Position trop à droite : devrait déclencher une exception HorsEspaceJeuException");
 		} catch (final HorsEspaceJeuException e) {
 		}
 		
 		
 		try {
-			spaceinvaders.positionnerUnNouveauVaisseau(-1,9);
+			spaceinvaders.positionnerUnNouveauVaisseau(1,1,-1,9);
 			fail("Position trop à gauche : devrait déclencher une exception HorsEspaceJeuException");
 		} catch (final HorsEspaceJeuException e) {
 		}
 		
 		
 		try {
-			spaceinvaders.positionnerUnNouveauVaisseau(14,10);
+			spaceinvaders.positionnerUnNouveauVaisseau(1,1,14,10);
 			fail("Position trop en bas : devrait déclencher une exception HorsEspaceJeuException");
 		} catch (final HorsEspaceJeuException e) {
 		}
 		
 		
 		try {
-			spaceinvaders.positionnerUnNouveauVaisseau(14,-1);
+			spaceinvaders.positionnerUnNouveauVaisseau(1,1,14,-1);
 			fail("Position trop à haut : devrait déclencher une exception HorsEspaceJeuException");
 		} catch (final HorsEspaceJeuException e) {
 		}
