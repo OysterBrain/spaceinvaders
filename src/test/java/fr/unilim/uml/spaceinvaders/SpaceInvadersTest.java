942a5e63bbda08d1c1b0dc7eb10e1c7057e69df2
Sandjay Thiagaradja <sandjay.thiagaradja@etu.unilim.fr>
2018-05-15 11:41:29 +0200

    Dimensionner le Vaisseau; Fonctionnalité 2 étape 2 fini(positionner un vaisseau avec une dimension)

--- src/test/java/fr/unilim/uml/spaceinvaders/SpaceInvadersTest.java
+++ src/test/java/fr/unilim/uml/spaceinvaders/SpaceInvadersTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.fail;
 import org.junit.Before;
 import org.junit.Test;
 
+import fr.unilim.iut.spaceinvaders.utils.DebordementEspaceJeuException;
 import fr.unilim.iut.spaceinvaders.utils.HorsEspaceJeuException;
 
 public class SpaceInvadersTest {
@@ -172,6 +173,23 @@ public class SpaceInvadersTest {
 			spaceinvaders.positionnerUnNouveauVaisseau(1,1,14,-1);
 			fail("Position trop à haut : devrait déclencher une exception HorsEspaceJeuException");
 		} catch (final HorsEspaceJeuException e) {
+		}
+			
+	}
+   @Test
+	public void test_UnNouveauVaisseauPositionneDansEspaceJeuMaisAvecDimensionTropGrande_DoitLeverUneExceptionDeDebordement() {
+		
+		try {
+			spaceinvaders.positionnerUnNouveauVaisseau(9,2,7,9);
+			fail("Dépassement du vaisseau à droite en raison de sa longueur trop importante : devrait déclencher une exception DebordementEspaceJeuException");
+		} catch (final DebordementEspaceJeuException e) {
+		}
+		
+		
+		try {
+			spaceinvaders.positionnerUnNouveauVaisseau(3,4,7,1);
+			fail("Dépassement du vaisseau vers le haut en raison de sa hauteur trop importante : devrait déclencher une exception DebordementEspaceJeuException");
+		} catch (final DebordementEspaceJeuException e) {
 		}
 			
 	}
